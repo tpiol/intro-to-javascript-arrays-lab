7864466cb56a4ba8de87ad18180dc0d3b10e24b4
Tia Piol <tiarox02@yahoo.com>
2025-06-18 15:35:52 -0700

    completed Exercise 7

--- app.js
+++ app.js
@@ -75,4 +75,18 @@ foods.splice (1, 1, 'sushi', 'cupcake')
 
 console.log('Exercise 6 result:', foods);
 
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+
+const yummy = foods.slice(1, 3);
+
+console.log('Exercise 7 result:', yummy);
 
