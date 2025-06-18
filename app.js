8bdebf1e165950ff3b8c4e7fd07dd99539e47a0b
Tia Piol <tiarox02@yahoo.com>
2025-06-18 16:13:54 -0700

    completed exercise 11

--- app.js
+++ app.js
@@ -137,3 +137,25 @@ const hasSoup = foods.includes('soup');
 
 
 console.log('Exercise 10 result:', hasSoup);
+
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+const odds = []
+for (let i = 0; i < nums.length; i++) {
+if (nums[i]%2 === 1) {
+    odds.push(nums[i])
+}
+}
+
+console.log('Exercise 11 result:', odds);
