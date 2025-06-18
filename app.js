2d7c632dc7faed957a3cd56e81fd5c70119cd69e
Tia Piol <tiarox02@yahoo.com>
2025-06-18 14:56:19 -0700

    completed exercise 3

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ foods.push('pizza', 'cheeseburger')
 
 console.log('Exercise 2 result:', foods);
 
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+foods.unshift('taco');
+
+console.log('Exercise 3 result:', foods);
 
 
 
