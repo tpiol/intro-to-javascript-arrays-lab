af1808206fe5b2ce85aee284fe4e68db983ec2e4
Tia Piol <tiarox02@yahoo.com>
2025-06-18 15:18:41 -0700

    complete exercise 5

--- app.js
+++ app.js
@@ -52,6 +52,19 @@ const  favFood = foods[1];
 
 console.log('Exercise 4 result:', favFood);
 
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice (2, 0, 'tofu')
+
+
+
+console.log('Exercise 5 result:', foods);
 
 
 
